0a7d414facfa199bae7d73de941616699714dac2
Teppo Kauppinen <eimink@gmail.com>
2018-01-20 18:37:02 +0200

    Cleanup and move to websockets for moving data to template.

--- TVNK/Form1.cs
+++ TVNK/Form1.cs
@@ -9,29 +9,42 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.Threading;
+using WebSocketSharp;
+using WebSocketSharp.Server;
 
 namespace TVNK
 {
     public partial class Form1 : Form
     {
         private TwitterService twitterService;
-        private CasparService casparService;
         private ConfigService configService;
         private Dictionary<int, Tweetinvi.Models.ITweet> tweetDict;
         private Thread tweetThread;
+        private WebSocketServer wssv;
+        private Tweetinvi.Models.ITweet selectedTweet;
 
         public Form1()
         {
             InitializeComponent();
             configService = new ConfigService();
             twitterService = new TwitterService(configService.settings.twitter,false);
-            casparService = new CasparService(configService.settings.caspar);
             tweetDict = new Dictionary<int, Tweetinvi.Models.ITweet>();
 
             tweetThread = new Thread(() => twitterService.StartMonitoring(configService.settings.twitter.searchString, HandleTweet));
             tweetThread.Start();
+            wssv = new WebSocketServer(14330);
+            wssv.Log.Level = LogLevel.Fatal;
+            wssv.AddWebSocketService<layerino.LayerinoWebSocket>("/tvnk");
+            wssv.Start();
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
         }
 
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            Debug.WriteLine("Form closing!");
+            twitterService.StopMonitoring(configService.settings.twitter.searchString, HandleTweet);
+            tweetThread.Abort();
+        }
 
         private void HandleTweet(object sender, Tweetinvi.Events.MatchedTweetReceivedEventArgs e)
         {
@@ -48,21 +61,40 @@ namespace TVNK
 
         private void tweetList_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            selectedTweet = tweetDict[tweetList.SelectedIndex];
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             if (tweetList.SelectedIndex >= 0)
             {
-                var t = tweetDict[tweetList.SelectedIndex];
+                selectedTweet = tweetDict[tweetList.SelectedIndex];
                 Dictionary<string, string> payload = new Dictionary<string, string>();
-                payload.Add("user", t.CreatedBy.Name);
-                payload.Add("tweet", t.FullText);
-                payload.Add("profileImage", t.CreatedBy.ProfileImageUrl400x400);
-                casparService.SendCommand(CasparCommand.Invoke(1, 10, "SetTweet('" + Newtonsoft.Json.JsonConvert.SerializeObject(payload)+"')"));
+                payload.Add("user", selectedTweet.CreatedBy.Name);
+                payload.Add("tweet", "@" + selectedTweet.CreatedBy.Name + ": " + selectedTweet.FullText);
+                payload.Add("profileImage", selectedTweet.CreatedBy.ProfileImageUrl400x400);
+                wssv.WebSocketServices.Broadcast(Newtonsoft.Json.JsonConvert.SerializeObject(payload));
             }
             
+        }
+
+        public void OnClientConnected()
+        {
+            if (this.IsHandleCreated)
+            {
+                Invoke(new Action(() =>
+                {
+                    if (selectedTweet != null)
+                    {
+                        selectedTweet = tweetDict[tweetList.SelectedIndex];
+                        Dictionary<string, string> payload = new Dictionary<string, string>();
+                        payload.Add("user", selectedTweet.CreatedBy.Name);
+                        payload.Add("tweet", "@"+ selectedTweet.CreatedBy.Name+": "+selectedTweet.FullText);
+                        payload.Add("profileImage", selectedTweet.CreatedBy.ProfileImageUrl400x400);
+                        wssv.WebSocketServices.Broadcast(Newtonsoft.Json.JsonConvert.SerializeObject(payload));
+                    }
+                }));
+            }
         }
     }
 }
--- TVNK/Program.cs
+++ TVNK/Program.cs
@@ -11,12 +11,17 @@ namespace TVNK
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// 
+
+        public static Form1 tvnkForm;
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            tvnkForm = new Form1();
+            Application.Run(tvnkForm);
         }
     }
 }
